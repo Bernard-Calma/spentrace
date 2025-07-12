8b25ea77ede86f1545844b2e936fc6f161316432
BC <nard.calma@gmail.com>
2025-07-11 20:34:44 -0700

    feat(Components): Common
    Create common folder for common components
    
    Create LabelInputComponent

--- app/demo/create-budget/page.tsx
+++ app/demo/create-budget/page.tsx
@@ -1,42 +1,37 @@
+import LabelInput from "@/common/LabelInput";
 import React from "react";
 
 function CreateBudgetPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h2>🧾 Create Your Budget</h2>
+      <h2 className="text-2xl font-bold">🧾 Create Your Budget</h2>
       <form action="" className="flex flex-col gap-4">
-        <label htmlFor="budgetName" className="font-semibold">
-          Budget Name:
-        </label>
-        <input
-          className="border border-gray-300 rounded p-2"
+        <LabelInput
           type="text"
-          id="budgetName"
+          htmlFor="budgetName"
+          text="Budget Name:"
+          name="budgetName"
           placeholder="Enter your budget name"
         />
-        <br />
-        <label htmlFor="owner" className="font-semibold">
-          Owner:
-        </label>
-        <input
-          className="border border-gray-300 rounded p-2"
+
+        <LabelInput
           type="text"
-          id="owner"
-          placeholder="Enter the owner's name"
+          htmlFor="owner"
+          text="Owner:"
+          name="owner"
+          placeholder="Enter your name"
         />
-        <br />
-        <label htmlFor="collaborators" className="font-semibold">
-          Collaborators:
-        </label>
-        <input
-          className="border border-gray-300 rounded p-2 disabled:bg-gray-100"
+
+        <LabelInput
           type="text"
-          id="collaborators"
+          htmlFor="collaborators"
+          text="Collaborators:"
+          name="collaborators"
           placeholder="Enter collaborator names"
           disabled
         />
         <p className="text-gray-500">Upgrade to invite others to this budget</p>
-        <br />
+
         <button
           type="submit"
           className="bg-blue-500 text-white rounded p-2 hover:bg-blue-600 transition-colors"
---
+++ common/LabelInput.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+interface LabelInputProps {
+  type: string;
+  htmlFor: string;
+  text: string;
+  name: string;
+  placeholder: string;
+  value?: string;
+  disabled?: boolean;
+}
+
+const LabelInput = ({
+  type,
+  htmlFor,
+  text,
+  name,
+  placeholder,
+  value,
+  disabled,
+}: LabelInputProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={htmlFor} className="font-semibold">
+        {text}
+      </label>
+      <input
+        className={`border border-gray-300 rounded p-2 ${
+          disabled ? "disabled:bg-gray-100" : ""
+        }`}
+        type={type}
+        id={htmlFor}
+        name={name}
+        placeholder={placeholder}
+        defaultValue={value}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
+
+export default LabelInput;

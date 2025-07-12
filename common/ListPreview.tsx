4d227ff4b05cf2db5a7bfdc1baff3e9b7a2e695a
BC <nard.calma@gmail.com>
2025-07-12 15:32:14 -0700

    feat(Recent Transactions): Add transactions
    map transcations to show listPreview

--- common/ListPreview.tsx
+++ common/ListPreview.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { format } from "date-fns";
 import Link from "next/link";
 
 import { usePathname } from "next/navigation";
@@ -21,24 +22,43 @@ const ListPreview = ({ length = 5, listItems = [] }: ListPreviewProps) => {
   return (
     <div className="list-preview w-full max-w-md p-4 bg-white rounded shadow">
       <div className="list-preview-header flex items-center justify-between mb-4">
-        <h2 className="text-lg">Recent Transactions</h2>
+        <h3 className="text-md font-bold">Recent Transactions</h3>
         {listItems.length ? (
-          <p className="text-sm text-gray-500">View All</p>
+          <p className="text-sm  text-gray-500 hover:text-blue-500 transition-colors">
+            View All
+          </p>
         ) : (
           <Link
             href={`${pathname}/add-transaction`}
-            className="text-sm text-gray-500"
+            className="text-sm text-gray-500 hover:text-blue-500 transition-colors"
           >
             Add a transaction
           </Link>
         )}
       </div>
       <ul>
-        {listItems.slice(0, length).map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.amount} ({item.type}) on {item.date}
-          </li>
-        ))}
+        {listItems.map((t, idx) =>
+          idx <= length - 1 ? (
+            <li
+              key={idx}
+              className="transaction-item flex items-center justify-between p-2 border-b last:border-b-0"
+            >
+              <div className="transaction-item_details">
+                <p className="transaction-item_name">{t.name}</p>
+                <p className="transaction-item_date text-sm text-gray-500">
+                  {format(t.date, "MMMM-dd")}
+                </p>
+              </div>
+              <p
+                className={`transaction-item_amount ${
+                  t.amount < 0 ? "expense" : "income"
+                }`}
+              >
+                {t.amount < 0 ? "-" : "+"}${Math.abs(t.amount).toFixed(2)}
+              </p>
+            </li>
+          ) : null
+        )}
       </ul>
     </div>
   );

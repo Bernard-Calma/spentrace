3a9b6f90fdbf6c5685ec78536f6a0726643bc5f8
BC <nard.calma@gmail.com>
2025-07-21 07:19:25 -0700

    feat(TransactionHeader): Menu
    Add previous transactions and filter for menu

--- app/demo/transactions/components/TransactionHeader.tsx
+++ app/demo/transactions/components/TransactionHeader.tsx
@@ -7,8 +7,10 @@ const TransactionHeader = ({
     <div className="transaction-header flex items-center justify-between p-4 bg-gray-100">
       <h2 className="text-md font-semibold">Transactions</h2>
       <div className="flex items-center space-x-4">
+        <p className="menu-previous cursor-pointer">🕘</p>
+        <p className="menu-filter cursor-pointer">🔽</p>
         <button
-          className="btn btn-primary p-1 bg-blue-500 hover:bg-blue-600 text-white text-sm rounded shadow transition-colors "
+          className="btn btn-primary p-1 bg-blue-500 hover:bg-blue-600 text-white text-sm rounded shadow transition-colors cursor-pointer"
           onClick={showAddTransaction}
         >
           + Add Transaction

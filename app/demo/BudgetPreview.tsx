a3479097902e03d8363e22450a36ab3449bae327
BC <nard.calma@gmail.com>
2025-07-22 03:10:19 -0700

    feat(BudgetPreview): components
    Add number of transactions indicator and next transaction data

--- app/demo/BudgetPreview.tsx
+++ app/demo/BudgetPreview.tsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from "react";
+import { format } from "date-fns";
 
 const BudgetPreview = ({ budget }: { budget: Budget }) => {
-  const [nextTransactionDue, setNextTransactionDue] =
-    useState<Transaction | null>(null);
+  const nextTransactionDue = budget.transactions.sort(
+    (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+  )[0];
 
-  useEffect(() => {
-    // Fetch budget data from localStorage
-
-    const handleSetNextTransactionDue = () => {
-      const today = new Date();
-      const upcomingTransactions = budget.transactions.filter(
-        (transaction) => new Date(transaction.date) > today
-      );
-      if (upcomingTransactions.length > 0) {
-        const nextTransaction = upcomingTransactions.reduce((prev, curr) =>
-          new Date(prev.date) < new Date(curr.date) ? prev : curr
-        );
-        setNextTransactionDue(nextTransaction);
-      } else {
-        setNextTransactionDue(null);
-      }
-    };
-    handleSetNextTransactionDue();
-  }, []);
   return (
     <div className="budget-preview relative w-full h-full text-xs flex flex-1 flex-col bg-gray-100 shadow-md rounded-lg p-4">
-      <h2 className="text-lg font-bold">{budget.budgetName}</h2>
+      <h2 className="text-5xl font-bold">{budget.budgetName}</h2>
       <p className="absolute top-2 right-2 text-gray-600 text-md hover:underline cursor-pointer">
-        View
+        View Budget
       </p>
-      <div className="next-transaction_due ">
+      <div className="total-transactions text-2xl font-semibold mb-2">
+        <p>
+          Number of transactions: <span>{budget.transactions.length}</span>
+        </p>
+      </div>
+      <div className="next-transaction_due text-sm text-gray-700">
         {nextTransactionDue ? (
           <div className="next-transaction flex flex-col">
-            <h3 className="text-md font-semibold">Next Transaction Due:</h3>
-            <p>
+            <h3 className="text-md font-semibold text-red-600">
+              {/* Show remaining days until due date */}
+              Next Transaction Due{" "}
+              {`(${Math.ceil(
+                (new Date(nextTransactionDue.date).getTime() -
+                  new Date().getTime()) /
+                  (1000 * 60 * 60 * 24) || 0
+              )} days)`}
+              :
+            </h3>
+            <p className="text-xl">
               {nextTransactionDue.name} -{" "}
-              {new Date(nextTransactionDue.date).toLocaleDateString()} - $
+              {format(nextTransactionDue.date, "MMMM dd")} - $
               {nextTransactionDue.amount}
             </p>
           </div>

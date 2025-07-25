5e75e4d55908d627b37029b5c18f1104579a23e8
BC <nard.calma@gmail.com>
2025-07-25 11:22:30 -0700

    feat(Transactio Header): Refresh
    Add Refresh Button

--- app/demo/transactions/components/TransactionHeader.tsx
+++ app/demo/transactions/components/TransactionHeader.tsx
@@ -6,8 +6,15 @@ const TransactionHeader = () => {
     <div className="transaction-header flex items-center justify-between p-4 bg-gray-100">
       <h2 className="text-md font-semibold">Transactions</h2>
       <div className="flex items-center space-x-4">
-        <p className="menu-previous cursor-pointer">🕘</p>
-        <p className="menu-filter cursor-pointer">🔽</p>
+        <p className="menu-refresh cursor-pointer" title={"refresh"}>
+          🔄
+        </p>
+        <p className="menu-previous cursor-pointer" title={"previous"}>
+          🕘
+        </p>
+        <p className="menu-filter cursor-pointer" title={"filter"}>
+          🔽
+        </p>
         <button
           className="btn btn-primary p-1 bg-blue-500 hover:bg-blue-600 text-white text-sm rounded shadow transition-colors cursor-pointer"
           onClick={() => router.push("add-transaction")}

93a3f48ed5605b928f6141a3100454977fcb43a1
BC <nard.calma@gmail.com>
2025-08-21 06:24:55 -0700

    feat(Bills): Header
    Add bills filter in header

--- app/demo/bills/components/header.tsx
+++ app/demo/bills/components/header.tsx
@@ -4,13 +4,38 @@ import { useState } from "react";
 
 const BillsHeader = () => {
   const today = new Date();
+  const [filter, setFilter] = useState("all");
   const [currentDate, setCurrentDate] = useState(
     new Date(today.getFullYear(), today.getMonth(), 1)
   );
 
   return (
     <header className="bills-header flex flex-col">
-      <h2>Bills</h2>
+      <div className="flex items-center justify-between">
+        <h2>Bills</h2>
+        <div className="flex justify-between gap-4">
+          <p
+            className={`cursor-pointer ${filter === "paid" ? "font-bold" : ""}`}
+            onClick={() => setFilter("paid")}
+          >
+            Paid
+          </p>
+          <p
+            className={`cursor-pointer ${
+              filter === "unpaid" ? "font-bold" : ""
+            }`}
+            onClick={() => setFilter("unpaid")}
+          >
+            Unpaid
+          </p>
+          <p
+            className={`cursor-pointer ${filter === "all" ? "font-bold" : ""}`}
+            onClick={() => setFilter("all")}
+          >
+            Show All
+          </p>
+        </div>
+      </div>
       <div className="month-selector flex items-center justify-between px-4">
         <button
           onClick={() =>

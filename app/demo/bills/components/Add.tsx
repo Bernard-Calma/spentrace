8399caff8f3d4fa49f98926b6059adbf337b4e33
BC <nard.calma@gmail.com>
2025-08-25 05:09:33 -0700

    feat(Add Bill): Inputs
    Add inputs

--- app/demo/bills/components/Add.tsx
+++ app/demo/bills/components/Add.tsx
@@ -1,15 +1,21 @@
 "use client";
 
+import { LabelInput } from "@/common";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const AddBill: React.FC<{ handleToggleAdd: () => void }> = ({
   handleToggleAdd,
 }) => {
+  const { isDemo } = useSelector((state: any) => state.user);
+
   const [newBill, setNewBill] = useState({
-    dueDate: Date.now(),
+    dueDate: new Date(),
     name: "",
     amount: 0,
+    category: "",
     paid: false,
+    notes: "",
   });
 
   const handleChange = (e: any) => {
@@ -25,53 +31,104 @@ const AddBill: React.FC<{ handleToggleAdd: () => void }> = ({
   };
   return (
     <div className="overlay flex items-center justify-center">
-      <div className="modal-content bg-white p-4 rounded shadow-lg">
-        <h2 className="text-2xl font-bold text-center">Add Bill</h2>
-        <form onSubmit={handleSubmitAddBill} className="p-4">
-          <label htmlFor="dueDate" className="text-md font-semibold">
-            Due Date
-          </label>
+      <form
+        onSubmit={handleSubmitAddBill}
+        className="add-transaction-form w-96 relative bg-white p-4 rounded-lg shadow-lg"
+      >
+        <button
+          onClick={handleToggleAdd}
+          className="close-button absolute top-0 right-2 text-red-500 hover:text-red-800 transition-colors text-5xl cursor-pointer font-bold"
+        >
+          &times;
+        </button>
+        <h2 className="text-lg font-semibold">Add Bill</h2>
+        <div className="input-amount flex items-center justify-center">
+          {" "}
+          <span className="text-5xl font-bold ">$</span>
           <input
-            type="date"
-            name="dueDate"
-            className="w-full border px-2 mb-2 rounded"
-            onChange={handleChange}
-          />
-          <label htmlFor="name" className="text-md font-semibold">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            className="w-full border px-2 mb-2 rounded"
-            onChange={handleChange}
-          />
-          <label htmlFor="amount" className="text-md font-semibold">
-            Amount
-          </label>
-          <input
+            className="field-sizing-content px-2 rounded text-5xl font-bold focus:outline-none appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
             type="number"
+            placeholder="0"
             name="amount"
-            className="w-full border px-2 mb-2 rounded"
+            pattern="[0-9]*"
+            // no negative
+            min="0"
+            step="0.01"
+            value={newBill.amount === 0 ? "" : newBill.amount}
+            onKeyDown={(e) => {
+              // prevent plus or minus signs
+              if (e.key === "+" || e.key === "-") {
+                e.preventDefault();
+              }
+            }}
+            autoComplete="off"
             onChange={handleChange}
+            required
           />
-          <div className="flex justify-center gap-2">
-            {" "}
-            <button
-              onClick={handleToggleAdd}
-              className="bg-red-500 hover:bg-red-400 text-white font-bold py-1 px-2 border-b-4 border-red-700 hover:border-red-500 rounded"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-            >
-              Add
-            </button>
-          </div>
-        </form>
-      </div>
+        </div>
+
+        <LabelInput
+          type="text"
+          htmlFor="transactionName"
+          text="Transaction Name"
+          name="name"
+          placeholder="Enter transaction name"
+          onChange={handleChange}
+          required={true}
+        />
+
+        <LabelInput
+          type="date"
+          htmlFor="transactionDate"
+          text="Date"
+          name="date"
+          placeholder="Select date"
+          onChange={handleChange}
+          required={true}
+        />
+
+        <LabelInput
+          disabled={isDemo} // Disable if not in demo mode
+          type="text"
+          htmlFor="transactionCategory"
+          text="Category"
+          name="category"
+          placeholder="Enter category"
+          value={newBill.category}
+          onChange={handleChange}
+        />
+
+        <label className="font-semibold">Notes (optional)</label>
+        <textarea
+          className={`border border-gray-300 rounded p-2 w-full ${
+            isDemo
+              ? "disabled:bg-gray-600 disabled:cursor-not-allowed disabled:text-gray-400 disabled:italic"
+              : ""
+          }`}
+          name="notes"
+          placeholder="Add notes (optional)"
+          value={newBill.notes}
+          onChange={handleChange}
+          rows={3}
+          disabled={isDemo} // Disable if not in demo mode
+          //   title="If disabled, login or subscribe to enable feature."
+        ></textarea>
+
+        <div className="flex justify-end mt-4 gap-2">
+          <button
+            type="submit"
+            className="btn btn-primary bg-blue-500 text-white rounded p-2 hover:bg-blue-600 transition-colors"
+          >
+            Add Bill
+          </button>
+          <button
+            onClick={handleToggleAdd}
+            className="btn btn-secondary mr-2 bg-gray-300 text-gray-800 rounded p-2 hover:bg-gray-400 transition-colors"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
   );
 };

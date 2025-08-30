e77ff1f5fb7ee27ff68574adc5a2b0f831e183d9
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-08-30 16:00:18 -0700

    fix(LandingPage): Client
    Change LandingPage from Client to Server

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-import { demoUser } from "@/store/features/userSlice";
-import { useRouter } from "next/navigation";
-import { useDispatch } from "react-redux";
 import {
   ComparePlans,
   FeatureSection,
@@ -12,15 +8,6 @@ import {
 } from "./components";
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  // Create a demo object
-  const handleCreateDemo = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    dispatch(demoUser());
-    router.push("/demo");
-  };
-
   return (
     <>
       <Header />
